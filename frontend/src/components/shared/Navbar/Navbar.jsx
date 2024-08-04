2e63bd7874fdae0d34984e4df49bb32759b1aa60
legendajar <harshsolanki989@gmail.com>
2024-08-05 00:45:37 +0530

    chore: Update Navbar component to dynamically render navigation links based on user role

--- frontend/src/components/shared/Navbar/Navbar.jsx
+++ frontend/src/components/shared/Navbar/Navbar.jsx
@@ -39,9 +39,24 @@ const Navbar = () => {
                 </Link>
                 <div className="flex items-center gap-12">
                     <ul className="flex font-medium items-center gap-5">
-                        <li><Link to='/'>Home</Link></li>
-                        <li><Link to='/jobs'>Jobs</Link></li>
-                        <li><Link to='/browse'>Browse</Link></li>
+                        {
+                            user && user.role === 'recruiter' ? (
+                                <>
+                                    <li>
+                                        <Link to='/recruiter/company'>Companies</Link>
+                                    </li>
+                                    <li>
+                                        <Link to='/recruiter/jobs'>Jobs</Link>
+                                    </li>
+                                </>
+                            ) : (
+                                <>
+                                    <li><Link to='/'>Home</Link></li>
+                                    <li><Link to='/jobs'>Jobs</Link></li>
+                                    <li><Link to='/browse'>Browse</Link></li>
+                                </>
+                            )
+                        }
                     </ul>
                     {
                         !user ? (
@@ -73,12 +88,22 @@ const Navbar = () => {
                                         </div>
                                     </div>
                                     <div className="flex flex-col gap-3 text-gray-600">
-                                        <div className="flex w-fit items-center gap-2 cursor-pointer">
-                                            <User2 />
-                                            <Button variant="link">
-                                                <Link to='/profile'>View Profile</Link>
-                                            </Button>
-                                        </div>
+                                        {
+                                            user && user.role === 'student' ?
+                                            (
+                                                <>
+                                                    <div className="flex w-fit items-center gap-2 cursor-pointer">
+                                                        <User2 />
+                                                        <Button variant="link">
+                                                            <Link to='/profile'>View Profile</Link>
+                                                        </Button>
+                                                    </div>
+                                                </>
+                                            ) : (
+                                                <>
+                                                </>
+                                            )
+                                        }
                                         <div className="flex w-fit items-center gap-2 cursor-pointer">
                                             <LogOut />
                                             <Button onClick={logoutHandler} variant="link">
